238dd0325497fa485968cf31d1272f5235d8d600
eragoncity <kacper3wrzesien@gmail.com>
2020-11-13 17:43:13 +0100

    BG img, sliders

---
+++ javascript/menu.js
@@ -0,0 +1,17 @@
+// * Rozmiar Kółka
+var circle_size = document.getElementById("circle_size");
+var circle_size_output = document.getElementById("size");
+circle_size_output.innerHTML = circle_size.value + "px";
+
+circle_size.oninput = function() {
+  circle_size_output.innerHTML = this.value + "px";
+};
+
+// * Czas na kliknięcie
+var time = document.getElementById("time");
+var time_output = document.getElementById("timeout");
+time_output.innerHTML = time.value + "s";
+
+time.oninput = function() {
+    time_output.innerHTML = this.value + "s";
+};

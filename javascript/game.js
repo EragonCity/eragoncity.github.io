f8b478ebf39e2d9768f0b21abdc1a0d78ff20b74
eragoncity <kacper3wrzesien@gmail.com>
2020-11-22 12:15:20 +0100

    playarea border

--- javascript/game.js
+++ javascript/game.js
@@ -1,12 +1,25 @@
-var time = localStorage.getItem("timeLocalStorage")
-var size = localStorage.getItem("circle_sizeLocalStorage")
-document.getElementById("debug").innerHTML = time + '<br>' + size;
+//*local storage
+var time_get = localStorage.getItem("timeLocalStorage");
+var size_get = localStorage.getItem("circle_sizeLocalStorage");
+
+//* string => number conversion
+var time = Number(time_get);
+var size = Number(size_get)/2;
+
+//*debug
+//!document.getElementById("debug").innerHTML = `${time}ms<br>${size}px`;
+
+//! back button
 function menu () {
     document.location.href = "index.html";
 };
 
-document.getElementById("button").style.padding= size;
+//* size apply
+document.getElementById("button").style.padding = size + "px";
 
-setInterval (function () {
-    
-}, 1000);
+setInterval (() => {
+    document.getElementById('playarea').style.width = `${window.innerWidth - 150}px`;
+    document.getElementById('playarea').style.height = `${window.innerHeight - 150}px`;
+    //*debug
+    //!document.getElementById('wh').innerHTML = `Debug<br>Width: ${window.innerWidth}<br>Height: ${window.innerHeight}`;
+  }, 0);

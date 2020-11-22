c69e5e17f6669d6b4f4652e885b5f3395ea93499
eragoncity <kacper3wrzesien@gmail.com>
2020-11-22 13:19:36 +0100

    position randomizer score viewing implemetation(WIP)

--- javascript/game.js
+++ javascript/game.js
@@ -20,6 +20,28 @@ document.getElementById("button").style.padding = size + "px";
 setInterval (() => {
     document.getElementById('playarea').style.width = `${window.innerWidth - 150}px`;
     document.getElementById('playarea').style.height = `${window.innerHeight - 150}px`;
+    document.getElementById('button').style.borderWidth = `${size/5}px`
     //*debug
     //!document.getElementById('wh').innerHTML = `Debug<br>Width: ${window.innerWidth}<br>Height: ${window.innerHeight}`;
-  }, 0);
+}, 0);
+
+//* position randomizer
+window.onload = pos;
+setInterval (() => {
+    var w = document.getElementById('playarea').offsetWidth;
+    var h = document.getElementById('playarea').offsetHeight;
+    var g = Math.floor(Math.random() * (w - size + 1)) + size;
+    var j = Math.floor(Math.random() * (h - size + 1)) + size;
+    document.getElementById('position').style.left = `${g}px`;
+    document.getElementById('position').style.top = `${j}px`;
+    //* debug
+    //! document.getElementById('wh').innerHTML = `Debug<br>X: ${g}<br>Y: ${j}`;
+}, time);
+
+//*score counting
+var score = 0;
+function click () {
+    score += 100;
+    const zeroPad = (num, places) => String(num).padStart(places, '0')
+    document.getElementById("score").innerHTML = zeroPad(score, 10);
+};
